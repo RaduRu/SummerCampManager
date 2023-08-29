b912e0c23d080b334301abc092a5fe3c1f588ba2
RaduRu <radu.ursu@stud.unifi.it>
2023-08-29 11:39:00 +0200

    ConnectionManager

--- main/Orm/ConnectionManager.java
+++ main/Orm/ConnectionManager.java
@@ -1,4 +1,19 @@
 package Orm;
+import java.sql.*;
 
 public class ConnectionManager {
+    private static final String url = "jdbc:postgresql://localhost:5432/UrsulottiDB";
+    private static final String username = "Radu";
+    private static final String password = "Moldovean2001";
+    private static Connection con = null;
+
+    private ConnectionManager(){}
+
+    static public Connection getConnection() throws SQLException {
+        if (con == null)
+            con = DriverManager.getConnection(url, username, password);
+
+        return con;
+    }
 }
+
